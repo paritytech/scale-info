8578484e205b4489ff0220d8580d0c6f5076d2e6
Shaopeng Wang <git@spwang.net>
2019-07-08 13:12:13 +1200

    RegisterSubtypes derive impl.

--- derive/src/register_subtypes.rs
+++ derive/src/register_subtypes.rs
@@ -14,8 +14,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use proc_macro2::TokenStream as TokenStream2;
-use syn::{self, parse::Result};
+use proc_macro2::{Span, TokenStream as TokenStream2};
+use quote::quote;
+use syn::{self, parse::Result, DeriveInput, parse_quote, Ident};
 
 pub fn generate(input: TokenStream2) -> TokenStream2 {
 	match generate_impl(input.into()) {
@@ -25,5 +26,41 @@ pub fn generate(input: TokenStream2) -> TokenStream2 {
 }
 
 pub fn generate_impl(input: TokenStream2) -> Result<TokenStream2> {
-	unimplemented!()
+	let mut ast: DeriveInput = syn::parse2(input)?;
+
+	// add bound
+	ast.generics.type_params_mut().for_each(|p| {
+		p.bounds.push(parse_quote!(_type_metadata::Metadata));
+	});
+
+	let ident = &ast.ident;
+	let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+	let register_types = ast.generics.type_params().into_iter().map(|ty| {
+		let ty_ident = ty.ident.clone();
+		quote! { registry.register_type::<#ty_ident>(); }
+	});
+
+	let register_subtypes_impl = quote! {
+		impl #impl_generics _type_metadata::RegisterSubtypes for #ident #ty_generics #where_clause {
+			fn register_subtypes(registry: &mut _type_metadata::Registry) {
+				#( #register_types )*
+			}
+		}
+	};
+
+	let mut renamed = String::from("_IMPL_REGISTER_SUBTYPES_FOR_");
+	renamed.push_str(ident.to_string().trim_start_matches("r#"));
+	let dummy_const = Ident::new(&renamed, Span::call_site());
+	let output = quote! {
+		#[allow(non_upper_case_globals, unused_attributes, unused_qualifications)]
+		const #dummy_const: () = {
+			#[allow(unknown_lints)]
+			#[cfg_attr(feature = "cargo-clippy", allow(useless_attribute))]
+			#[allow(rust_2018_idioms)]
+			use type_metadata as _type_metadata;
+			#register_subtypes_impl;
+		};
+	};
+
+	Ok(output.into())
 }
