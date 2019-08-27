b7b236cae6e11931ba4585a356a73ae33287d7e4
Robin Freyler <robin.freyler@gmail.com>
2019-08-27 10:29:41 +0200

    add doc comments to registry

--- src/registry.rs
+++ src/registry.rs
@@ -14,6 +14,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! The registry has the purpose to compactify types and strings found in type
+//! definitions and identifiers such as symbol names.
+//!
+//! This is done by deduplicating common strings and types in order to reuse
+//! their definitions which can grow arbitrarily large. A type is uniquely
+//! identified by its type identifier that is therefore used to refer to types
+//! and their definitions.
+//!
+//! Since symbol names etc. are often shared between different types they are
+//! as well deduplicated.
+//!
+//! Types with the same name are uniquely identifiable by introducing namespaces.
+//! For this the normal Rust namespace of a type is used where it has been defined it.
+//! Rust prelude types live within the so-called root namespace that is just empty.
+//! In general namespaces are ordered sequences of symbols and thus also profit from
+//! string deduplication.
+
 use crate::tm_std::*;
 use crate::{
 	form::CompactForm,
@@ -23,24 +40,53 @@ use crate::{
 };
 use serde::Serialize;
 
+/// Compacts the implementor using a registry.
 pub trait IntoCompact {
+	/// The compact version of `Self`.
 	type Output;
 
+	/// Compacts `self` by using the registry for caching and compaction.
 	fn into_compact(self, registry: &mut Registry) -> Self::Output;
 }
 
+/// The pair of associated type identifier and structure.
+///
+/// This exists only as compactified version and is part of the registry.
 #[derive(Debug, PartialEq, Eq, Serialize)]
 pub struct TypeIdDef {
+	/// The identifier of the type.
 	id: TypeId<CompactForm>,
+	/// The definition (aka internal structure) of the type.
 	def: TypeDef<CompactForm>,
 }
 
+/// The registry for compaction of type identifiers and definitions.
+///
+/// The registry consists of a cache for strings such as symbol names
+/// and a cache for already compactified type identifiers and definitions.
+///
+/// Whenever using the registry to compact a type all of its sub-types
+/// are going to be registered recursively as well. A type is a sub-type
+/// of another type if it is used by its identifier or structure.
+///
+/// # Note
+///
+/// A type can be a sub-type of itself. In this case the registry has a builtin
+/// mechanism to stop recursion before going into an infinite loop.
 #[derive(Debug, PartialEq, Eq, Serialize)]
 pub struct Registry {
+	/// The cache for already registered strings.
 	#[serde(rename = "strings")]
 	string_table: Interner<&'static str>,
+	/// The cache for already registered types.
+	///
+	/// This is just an accessor to the actual database
+	/// for all types found in the `types` field.
 	#[serde(skip)]
 	type_table: Interner<AnyTypeId>,
+	/// The database where registered types actually reside.
+	///
+	/// This is going to be serialized upon serlialization.
 	#[serde(serialize_with = "serialize_registry_types")]
 	types: BTreeMap<UntrackedSymbol<core::any::TypeId>, TypeIdDef>,
 }
