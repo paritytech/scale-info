254fee129b5a66b7a61998654b5f04ece22c4ec7
David Palm <dvdplm@gmail.com>
2020-12-14 12:47:33 +0100

    Obey the fmt

--- src/registry.rs
+++ src/registry.rs
@@ -206,7 +206,10 @@ impl Registry {
 /// A read-only registry, to be used for decoding/deserializing
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Debug, PartialEq, Eq, Decode)]
-#[cfg_attr(feature = "serde", serde(bound(serialize = "S: Serialize", deserialize = "S: DeserializeOwned")))]
+#[cfg_attr(
+    feature = "serde",
+    serde(bound(serialize = "S: Serialize", deserialize = "S: DeserializeOwned"))
+)]
 pub struct RegistryReadOnly<S = &'static str>
 where
     S: PartialEq + Eq + PartialOrd + Ord + Clone + Debug,
