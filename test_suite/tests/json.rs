6c11f8f0e8200c1479a30574b4673680b030f979
David Palm <dvdplm@gmail.com>
2021-01-08 00:36:51 +0100

    Remove todo

--- test_suite/tests/json.rs
+++ test_suite/tests/json.rs
@@ -135,7 +135,6 @@ fn test_builtins() {
     assert_json_for_type::<str>(json!({ "def": { "primitive": "str" } }));
     // PhantomData
     assert_json_for_type::<PhantomData<bool>>(json!({
-        // TODO: dp – Not sure this is right, doesn't seem very useful?
         "path": ["PhantomData"],
         "def": { "phantom": { "type": 1 } },
         "params": [1]
