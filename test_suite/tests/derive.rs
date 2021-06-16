48f5e2f5cd74dcdddc0fe02b00b0f1746acd0d29
Andrew Jones <ascjones@gmail.com>
2021-06-16 14:01:53 +0100

    Use wildcard for `compile_fail` tests (#97)
    * Remove outdated HasCompact comment
    
    * Add wildcard for compile_fail tests
    
    * Fix wildcard to skip stderr files
    
    * Remove invalid test, there is an equivalent `pass` test.

--- test_suite/tests/derive.rs
+++ test_suite/tests/derive.rs
@@ -613,9 +613,6 @@ fn doc_capture_works() {
 #[test]
 fn ui_tests() {
     let t = trybuild::TestCases::new();
-    t.compile_fail("tests/ui/fail_missing_derive.rs");
-    t.compile_fail("tests/ui/fail_unions.rs");
-    t.compile_fail("tests/ui/fail_use_codec_attrs_without_deriving_encode.rs");
-    t.compile_fail("tests/ui/fail_with_invalid_codec_attrs.rs");
+    t.compile_fail("tests/ui/fail_*.rs");
     t.pass("tests/ui/pass_*");
 }
