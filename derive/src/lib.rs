11c730f75d33aea07ee7d6bfa78e051159aeb897
Andrew Jones <ascjones@gmail.com>
2021-06-23 16:21:14 +0100

    Add `docs` feature (#101)
    * WIP add docs feature
    
    * Add `docs` feature
    
    * Make `docs` a default feature
    
    * Update Cargo.toml
    
    Co-authored-by: David <dvdplm@gmail.com>
    
    Co-authored-by: David <dvdplm@gmail.com>

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -104,7 +104,7 @@ fn generate_type(input: TokenStream2) -> Result<TokenStream2> {
         Data::Enum(ref e) => generate_variant_type(e, &scale_info),
         Data::Union(_) => return Err(Error::new_spanned(input, "Unions not supported")),
     };
-    let docs = utils::get_doc_literals(&ast.attrs);
+    let docs = generate_docs(&ast.attrs);
 
     let type_info_impl = quote! {
         impl #impl_generics :: #scale_info ::TypeInfo for #ident #ty_generics #where_clause {
@@ -113,7 +113,7 @@ fn generate_type(input: TokenStream2) -> Result<TokenStream2> {
                 :: #scale_info ::Type::builder()
                     .path(:: #scale_info ::Path::new(::core::stringify!(#ident), ::core::module_path!()))
                     .type_params(:: #scale_info ::prelude::vec![ #( #generic_type_ids ),* ])
-                    .docs(&[ #( #docs ),* ])
+                    #docs
                     .#build_type
             }
         }
@@ -160,7 +160,7 @@ fn generate_fields(fields: &FieldsList) -> Vec<TokenStream2> {
             StaticLifetimesReplace.visit_type_mut(&mut ty);
 
             let type_name = clean_type_string(&quote!(#ty).to_string());
-            let docs = utils::get_doc_literals(&f.attrs);
+            let docs = generate_docs(&f.attrs);
             let type_of_method = if utils::is_compact(f) {
                 quote!(compact)
             } else {
@@ -176,7 +176,7 @@ fn generate_fields(fields: &FieldsList) -> Vec<TokenStream2> {
                     .#type_of_method::<#ty>()
                     #name
                     .type_name(#type_name)
-                    .docs(&[ #( #docs ),* ])
+                    #docs
                 )
             )
         })
@@ -235,12 +235,12 @@ fn generate_c_like_enum_def(variants: &VariantList, scale_info: &Ident) -> Token
         .map(|(i, v)| {
             let name = &v.ident;
             let discriminant = utils::variant_index(v, i);
-            let docs = utils::get_doc_literals(&v.attrs);
+            let docs = generate_docs(&v.attrs);
             quote! {
                 .variant(::core::stringify!(#name), |v|
                     v
                         .discriminant(#discriminant as ::core::primitive::u64)
-                        .docs(&[ #( #docs ),* ])
+                        #docs
                 )
             }
         });
@@ -272,7 +272,7 @@ fn generate_variant_type(data_enum: &DataEnum, scale_info: &Ident) -> TokenStrea
         .map(|v| {
             let ident = &v.ident;
             let v_name = quote! {::core::stringify!(#ident) };
-            let docs = utils::get_doc_literals(&v.attrs);
+            let docs = generate_docs(&v.attrs);
             let index = utils::maybe_index(v).map(|i| quote!(.index(#i)));
 
             let fields = match v.fields {
@@ -301,7 +301,7 @@ fn generate_variant_type(data_enum: &DataEnum, scale_info: &Ident) -> TokenStrea
                 .variant(#v_name, |v|
                     v
                         .fields(#fields)
-                        .docs(&[ #( #docs ),* ])
+                        #docs
                         #index
                 )
             }
@@ -312,4 +312,36 @@ fn generate_variant_type(data_enum: &DataEnum, scale_info: &Ident) -> TokenStrea
                 #( #variants )*
         )
     }
+}
+
+#[cfg(feature = "docs")]
+fn generate_docs(attrs: &[syn::Attribute]) -> Option<TokenStream2> {
+    let docs = attrs
+        .iter()
+        .filter_map(|attr| {
+            if let Ok(syn::Meta::NameValue(meta)) = attr.parse_meta() {
+                if meta.path.get_ident().map_or(false, |ident| ident == "doc") {
+                    let lit = &meta.lit;
+                    let doc_lit = quote!(#lit).to_string();
+                    let trimmed_doc_lit =
+                        doc_lit.trim_start_matches(r#"" "#).trim_end_matches('"');
+                    let lit: syn::Lit = parse_quote!(#trimmed_doc_lit);
+                    Some(lit)
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    Some(quote! {
+        .docs(&[ #( #docs ),* ])
+    })
+}
+
+#[cfg(not(feature = "docs"))]
+fn generate_docs(_: &[syn::Attribute]) -> Option<TokenStream2> {
+    None
 }
