5d70abb81014ce343ebeb09c992340d96c013364
Alexandru Vasile <alexandru.vasile@parity.io>
2023-03-17 13:30:36 +0200

    ty: Implement zero-alloc Default::default() for Type
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- src/ty/mod.rs
+++ src/ty/mod.rs
@@ -86,6 +86,22 @@ pub struct Type<T: Form = MetaForm> {
     pub docs: Vec<T::String>,
 }
 
+impl<T> Default for Type<T>
+where
+    T: Form,
+{
+    // Zero-allocation default implementation that assumes
+    // the type def to be `TypeDefPrimitive::Bool`.
+    fn default() -> Type<T> {
+        Type {
+            type_def: TypeDef::Primitive(TypeDefPrimitive::Bool),
+            path: Path::default(),
+            type_params: vec![],
+            docs: vec![],
+        }
+    }
+}
+
 impl IntoPortable for Type {
     type Output = Type<PortableForm>;
 
