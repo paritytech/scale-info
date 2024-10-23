14d67c40f4d50fca52c34c31b058c9160d13ae62
James Wilson <james.wilson@parity.io>
2024-10-23 10:04:13 +0100

    Remove non-existant feature flag from tests

--- test_suite/tests/codec.rs
+++ test_suite/tests/codec.rs
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![no_std]
 #![allow(unused)]
-#![cfg_attr(not(feature = "std"), no_std)]
 #![allow(dead_code)]
 
 use info::{self as scale_info};
